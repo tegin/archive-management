1b9d2092dcc6ab20b94d65ab1ff0afb250682215
Enric Tobella <etobella@creublanca.es>
2018-10-23 13:20:26 +0200

    [IMP] Add report for reviewing all the movement history of a file and storage

--- archive_management/models/archive_transfer.py
+++ archive_management/models/archive_transfer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ArchiveFileTransfer(models.Model):
@@ -12,6 +12,21 @@ class ArchiveFileTransfer(models.Model):
     dest_storage_id = fields.Many2one('archive.storage', readonly=True)
     src_partner_id = fields.Many2one('res.partner', readonly=True)
     dest_partner_id = fields.Many2one('res.partner', readonly=True)
+
+    @api.multi
+    def get_transfer_report(self):
+        self.ensure_one()
+        res = {
+            'source': self.file_id.name,
+            'date': self.create_date,
+            'user_id': self.create_uid.name,
+            'partner_id': self.dest_partner_id.name or False,
+            'location_id': False,
+            'storage_id': False,
+        }
+        if self.dest_storage_id:
+            res.update(self.dest_storage_id._position_vals(self.create_date))
+        return res
 
 
 class ArchiveStorageTransfer(models.Model):
@@ -27,3 +42,18 @@ class ArchiveStorageTransfer(models.Model):
     dest_partner_id = fields.Many2one('res.partner', readonly=True)
     src_location_id = fields.Many2one('archive.location', readonly=True)
     dest_location_id = fields.Many2one('archive.location', readonly=True)
+
+    @api.multi
+    def get_transfer_report(self):
+        self.ensure_one()
+        res = {
+            'source': self.storage_id.name,
+            'date': self.create_date,
+            'user_id': self.create_uid.name,
+            'storage_id': False,
+            'partner_id': self.dest_partner_id.name or False,
+            'location_id': self.dest_location_id.name or False,
+        }
+        if self.dest_storage_id:
+            res.update(self.dest_storage_id._position_vals(self.create_date))
+        return res
---
+++ archive_management/report/__init__.py
@@ -0,0 +1,1 @@
+from . import report_transfer_history
