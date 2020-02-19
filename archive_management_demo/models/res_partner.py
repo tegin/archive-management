ac07391d5e69db4f5814a8c4b62aa8109ecc72df
Enric Tobella <etobella@creublanca.es>
2020-02-19 17:46:41 +0100

    Blacking all

--- archive_management_demo/models/res_partner.py
+++ archive_management_demo/models/res_partner.py
@@ -2,5 +2,5 @@ from odoo import models
 
 
 class ResPartner(models.Model):
-    _name = 'res.partner'
-    _inherit = ['res.partner', 'archive.abstract']
+    _name = "res.partner"
+    _inherit = ["res.partner", "archive.abstract"]
