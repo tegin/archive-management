00199e144f972711de26427f6d8734c2b76f54c7
Enric Tobella <etobella@creublanca.es>
2020-02-19 17:46:41 +0100

    Blacking all

--- archive_management/wizards/archive_file_add.py
+++ archive_management/wizards/archive_file_add.py
@@ -2,53 +2,61 @@ from odoo import api, fields, models
 
 
 class ArchiveFileAdd(models.TransientModel):
-    _name = 'archive.file.add'
-    _description = 'archive.file.add'
+    _name = "archive.file.add"
+    _description = "archive.file.add"
 
     res_model = fields.Char(required=True, readonly=True)
     res_id = fields.Integer(required=True, readonly=True)
     repository_id = fields.Many2one(
-        'archive.repository',
-        string='Repository',
-        domain="[('id', 'in', repository_ids)]"
+        "archive.repository",
+        string="Repository",
+        domain="[('id', 'in', repository_ids)]",
     )
     repository_ids = fields.Many2many(
-        'archive.repository',
-        string='Repositories',
-        compute='_compute_repositories'
+        "archive.repository",
+        string="Repositories",
+        compute="_compute_repositories",
     )
 
-    @api.depends('res_model', 'res_id')
+    @api.depends("res_model", "res_id")
     def _compute_repositories(self):
         for record in self:
-            repos = self.env['archive.file'].search([
-                ('res_model', '=', record.res_model),
-                ('res_id', '=', record.res_id)
-            ]).mapped('repository_id')
-            res_model = self.env['ir.model'].search([('model', '=',
-                                                      record.res_model)])
-            record.repository_ids = self.env['archive.repository'].search([
-                ('res_model_ids', '=', res_model.id),
-                ('id', 'not in', repos.ids)
-            ])
+            repos = (
+                self.env["archive.file"]
+                .search(
+                    [
+                        ("res_model", "=", record.res_model),
+                        ("res_id", "=", record.res_id),
+                    ]
+                )
+                .mapped("repository_id")
+            )
+            res_model = self.env["ir.model"].search(
+                [("model", "=", record.res_model)]
+            )
+            record.repository_ids = self.env["archive.repository"].search(
+                [
+                    ("res_model_ids", "=", res_model.id),
+                    ("id", "not in", repos.ids),
+                ]
+            )
 
     def _file_vals(self):
         return {
-            'res_model': self.res_model,
-            'res_id': self.res_id,
-            'repository_id': self.repository_id.id,
+            "res_model": self.res_model,
+            "res_id": self.res_id,
+            "repository_id": self.repository_id.id,
         }
 
     def _run(self):
-        return self.env['archive.file'].create(self._file_vals())
+        return self.env["archive.file"].create(self._file_vals())
 
     def run(self):
         self.ensure_one()
         doc = self._run()
-        action = self.env.ref(
-            'archive_management.archive_file_action')
+        action = self.env.ref("archive_management.archive_file_action")
         result = action.read()[0]
-        res = self.env.ref('archive_management.archive_file_form', False)
-        result['views'] = [(res and res.id or False, 'form')]
-        result['res_id'] = doc.id
+        res = self.env.ref("archive_management.archive_file_form", False)
+        result["views"] = [(res and res.id or False, "form")]
+        result["res_id"] = doc.id
         return result
