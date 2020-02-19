8e00018b775ad882fc3876e16b3b80acdb389b58
Enric Tobella <etobella@creublanca.es>
2020-02-19 17:46:41 +0100

    Blacking all

--- archive_management/models/archive_storage.py
+++ archive_management/models/archive_storage.py
@@ -3,111 +3,94 @@ from odoo.exceptions import ValidationError
 
 
 class ArchiveStorage(models.Model):
-    _name = 'archive.storage'
-    _description = 'archive.storage'
-    _parent_name = 'storage_id'
+    _name = "archive.storage"
+    _description = "archive.storage"
+    _parent_name = "storage_id"
 
-    name = fields.Char(
-        required=True,
-        default='/',
-        readonly=True,
+    name = fields.Char(required=True, default="/", readonly=True)
+    state = fields.Selection(
+        [
+            ("draft", "Draft"),
+            ("on_place", "On Place"),
+            ("destroyed", "Destroyed"),
+        ],
+        default="draft",
     )
-    state = fields.Selection([
-        ('draft', 'Draft'),
-        ('on_place', 'On Place'),
-        ('destroyed', 'Destroyed')
-    ], default='draft')
     parent_ids = fields.One2many(
-        'archive.storage.parent',
-        inverse_name='child_id',
-        string='Parents',
+        "archive.storage.parent",
+        inverse_name="child_id",
+        string="Parents",
         readonly=True,
     )
     parent_id = fields.Many2one(
-        'archive.storage.parent',
-        string='Parent',
-        compute='_compute_parent',
+        "archive.storage.parent",
+        string="Parent",
+        compute="_compute_parent",
         store=True,
         readonly=True,
     )
     storage_id = fields.Many2one(
-        'archive.storage',
-        compute='_compute_parent',
-        store=True,
-        readonly=True,
+        "archive.storage", compute="_compute_parent", store=True, readonly=True
     )
-    parent_left = fields.Integer(
-        'Left Parent',
-        index=True,
-    )
-    parent_right = fields.Integer(
-        'Right Parent',
-        index=True,
-    )
+    parent_left = fields.Integer("Left Parent", index=True)
+    parent_right = fields.Integer("Right Parent", index=True)
     child_ids = fields.One2many(
-        'archive.storage',
-        inverse_name='storage_id',
-        readonly=True,
+        "archive.storage", inverse_name="storage_id", readonly=True
     )
     location_id = fields.Many2one(
-        'archive.location',
-        compute='_compute_parent',
+        "archive.location",
+        compute="_compute_parent",
         store=True,
         readonly=True,
     )
     partner_id = fields.Many2one(
-        'res.partner',
-        compute='_compute_parent',
-        store=True,
-        readonly=True
+        "res.partner", compute="_compute_parent", store=True, readonly=True
     )
     current_partner_id = fields.Many2one(
-        'res.partner',
-        compute='_compute_current_partner',
+        "res.partner",
+        compute="_compute_current_partner",
         store=True,
-        readonly=True
+        readonly=True,
     )
     current_location_id = fields.Many2one(
-        'archive.location',
-        store=True,
-        compute='_compute_current_location',
+        "archive.location", store=True, compute="_compute_current_location"
     )
     file_ids = fields.One2many(
-        'archive.file',
-        inverse_name='storage_id',
-        readonly=True,
+        "archive.file", inverse_name="storage_id", readonly=True
     )
     expected_destruction_date = fields.Datetime()
     destruction_date = fields.Datetime(readonly=True)
-    destruction_user_id = fields.Many2one('res.users', readonly=True)
+    destruction_user_id = fields.Many2one("res.users", readonly=True)
     repository_id = fields.Many2one(
-        'archive.repository', readonly=True,
-        related='repository_level_id.repository_id', store=True,
+        "archive.repository",
+        readonly=True,
+        related="repository_level_id.repository_id",
+        store=True,
     )
     repository_level_id = fields.Many2one(
-        'archive.repository.level',
-        required=True, readonly=True,
-        states={'draft': [('readonly', False)]}
+        "archive.repository.level",
+        required=True,
+        readonly=True,
+        states={"draft": [("readonly", False)]},
     )
     level = fields.Integer(
-        related='repository_level_id.level',
-        readonly=True,
-        store=True,
+        related="repository_level_id.level", readonly=True, store=True
     )
     can_assign_files = fields.Boolean(
-        related='repository_level_id.can_assign_files',
+        related="repository_level_id.can_assign_files",
         readonly=True,
         store=True,
     )
-    active = fields.Boolean(compute='_compute_active', store=True)
+    active = fields.Boolean(compute="_compute_active", store=True)
 
-    @api.constrains('storage_id')
+    @api.constrains("storage_id")
     def _check_recursion_parent_id(self):
         if not self._check_recursion():
             raise ValidationError(
-                _('Error! You are attempting to create a recursive storage.'))
+                _("Error! You are attempting to create a recursive storage.")
+            )
 
-    @api.depends('parent_ids')
+    @api.depends("parent_ids")
     def _compute_parent(self):
         for rec in self:
             parent = rec.parent_ids.filtered(lambda r: not r.end_date)
@@ -116,7 +99,7 @@ class ArchiveStorage(models.Model):
             rec.partner_id = parent.partner_id
             rec.location_id = parent.location_id
 
-    @api.depends('location_id', 'storage_id.current_location_id')
+    @api.depends("location_id", "storage_id.current_location_id")
     def _compute_current_location(self):
         for r in self:
             if r.location_id:
@@ -124,7 +107,7 @@ class ArchiveStorage(models.Model):
             else:
                 r.current_location_id = r.storage_id.current_location_id
 
-    @api.depends('partner_id', 'storage_id.current_partner_id')
+    @api.depends("partner_id", "storage_id.current_partner_id")
     def _compute_current_partner(self):
         for r in self:
             if r.partner_id:
@@ -132,82 +115,81 @@ class ArchiveStorage(models.Model):
             else:
                 r.current_partner_id = r.storage_id.current_partner_id
 
-    @api.depends('destruction_date')
+    @api.depends("destruction_date")
     def _compute_active(self):
         for r in self:
-            r.active = not (r.state == 'destroyed')
+            r.active = not (r.state == "destroyed")
 
     @api.model
     def create(self, vals):
-        if vals.get('name', '/') == '/':
-            vals['name'] = self.default_archive_name(vals)
-        if not vals.get('parent_ids', False):
-            vals['parent_ids'] = [(0, 0, {})]
+        if vals.get("name", "/") == "/":
+            vals["name"] = self.default_archive_name(vals)
+        if not vals.get("parent_ids", False):
+            vals["parent_ids"] = [(0, 0, {})]
         return super().create(vals)
 
     @api.model
     def default_archive_name(self, vals):
-        level = self.env['archive.repository.level'].browse(vals.get(
-            'repository_level_id', False
-        ))
+        level = self.env["archive.repository.level"].browse(
+            vals.get("repository_level_id", False)
+        )
         if level.sequence_id:
             return level.sequence_id._next()
-        return self.env['ir.sequence'].next_by_code(
-            'archive.storage') or '/'
+        return self.env["ir.sequence"].next_by_code("archive.storage") or "/"
 
-    @api.constrains('repository_id', 'file_ids')
+    @api.constrains("repository_id", "file_ids")
     def _check_repository_files(self):
         for rec in self:
             if rec.file_ids.filtered(
                 lambda r: r.repository_id != rec.repository_id
             ):
-                raise ValidationError(_(
-                    'Repository cannot be changed if files are assigned'
-                ))
+                raise ValidationError(
+                    _("Repository cannot be changed if files are assigned")
+                )
 
-    @api.constrains('repository_id', 'child_ids')
+    @api.constrains("repository_id", "child_ids")
     def _check_repository_childs(self):
         for rec in self:
             if rec.child_ids.filtered(
                 lambda r: r.repository_id != rec.repository_id
             ):
-                raise ValidationError(_(
-                    'Repository cannot be changed if files are assigned'
-                ))
+                raise ValidationError(
+                    _("Repository cannot be changed if files are assigned")
+                )
 
-    @api.constrains('repository_id', 'storage_id')
+    @api.constrains("repository_id", "storage_id")
     def _check_repository_storage(self):
         if self.filtered(
-            lambda r: r.storage_id and
-            r.storage_id.repository_id != r.repository_id
+            lambda r: r.storage_id
+            and r.storage_id.repository_id != r.repository_id
         ):
-            raise ValidationError(_(
-                'Repository cannot be changed if it is assigned to a storage'
-            ))
+            raise ValidationError(
+                _(
+                    "Repository cannot be changed if it is assigned to a storage"
+                )
+            )
 
     @api.model
     def _destroy_vals(self):
         return {
-            'destruction_date': fields.Datetime.now(),
-            'state': 'destroyed',
-            'destruction_user_id': self.env.user.id,
+            "destruction_date": fields.Datetime.now(),
+            "state": "destroyed",
+            "destruction_user_id": self.env.user.id,
         }
 
     def _transfer(self, transfer):
         self.parent_id.close(transfer)
-        if self.state == 'draft':
-            self.write({'state': 'on_place'})
+        if self.state == "draft":
+            self.write({"state": "on_place"})
         if transfer:
-            self.env['archive.storage.parent'].create({
-                'child_id': self.id,
-                'transfer_id': transfer.id,
-            })
+            self.env["archive.storage.parent"].create(
+                {"child_id": self.id, "transfer_id": transfer.id}
+            )
 
     @api.multi
     def destroy(self):
         for rec in self:
-            rec.file_ids.filtered(
-                lambda r: not r.destruction_date).destroy()
+            rec.file_ids.filtered(lambda r: not r.destruction_date).destroy()
             rec.parent_id.close()
             rec.child_ids.destroy()
         self.write(self._destroy_vals())
@@ -219,106 +201,100 @@ class ArchiveStorage(models.Model):
         start = fields.Datetime.from_string(start_date or self.create_date)
         end = fields.Datetime.from_string(end_date or fields.Datetime.now())
         if not start_date:
-            data.append({
-                'source': self.name,
-                'date': self.create_date,
-                'message': _('Creation'),
-                'user_id': self.create_uid.name
-            })
+            data.append(
+                {
+                    "source": self.name,
+                    "date": self.create_date,
+                    "message": _("Creation"),
+                    "user_id": self.create_uid.name,
+                }
+            )
         for p in self.parent_ids.filtered(
             lambda r: (
-                not r.end_date or
-                fields.Datetime.from_string(r.end_date) >= start
-            ) and fields.Datetime.from_string(r.start_date) <= end
-
+                not r.end_date
+                or fields.Datetime.from_string(r.end_date) >= start
+            )
+            and fields.Datetime.from_string(r.start_date) <= end
         ):
-            if p.transfer_id and fields.Datetime.from_string(
-                p.start_date
-            ) >= start:
+            if (
+                p.transfer_id
+                and fields.Datetime.from_string(p.start_date) >= start
+            ):
                 data.append(p.transfer_id.get_transfer_report())
             if p.parent_id:
                 data += p.parent_id.get_transfers(p.start_date, p.end_date)
         if not end_date and self.destruction_date:
-            data.append({
-                'source': self.name,
-                'date': self.destruction_date,
-                'message': _('Destruction'),
-                'user_id': self.destruction_user_id.name,
-            })
+            data.append(
+                {
+                    "source": self.name,
+                    "date": self.destruction_date,
+                    "message": _("Destruction"),
+                    "user_id": self.destruction_user_id.name,
+                }
+            )
         return data
 
     def _position_vals(self, date):
         datetime = fields.Datetime.from_string(date)
         parent = self.parent_ids.filtered(
             lambda r: (
-                not r.end_date or
-                fields.Datetime.from_string(r.end_date) <= datetime
-            ) and fields.Datetime.from_string(r.start_date) >= datetime
+                not r.end_date
+                or fields.Datetime.from_string(r.end_date) <= datetime
+            )
+            and fields.Datetime.from_string(r.start_date) >= datetime
         )
         if parent.parent_id:
             res = parent.parent_id._position_vals(date)
-            res['storage_id'] = '%s/%s' % (self.name, res['storage_id'])
+            res["storage_id"] = "%s/%s" % (self.name, res["storage_id"])
         else:
             res = {
-                'storage_id': self.name,
-                'partner_id': parent.partner_id.name or False,
-                'location_id': parent.location_id.name or False,
+                "storage_id": self.name,
+                "partner_id": parent.partner_id.name or False,
+                "location_id": parent.location_id.name or False,
             }
         return res
 
     @api.multi
     def print_transfer_history(self):
         return self.env.ref(
-            'archive_management.action_report_transfer_history_file'
-        ).report_action(self.ids, data={'res_model': self._name})
+            "archive_management.action_report_transfer_history_file"
+        ).report_action(self.ids, data={"res_model": self._name})
 
 
 class ArchiveStorageParent(models.Model):
-    _name = 'archive.storage.parent'
-    _description = 'archive.storage.parent'
+    _name = "archive.storage.parent"
+    _description = "archive.storage.parent"
 
-    child_id = fields.Many2one(
-        'archive.storage',
-        required=True,
-        readonly=True,
-    )
+    child_id = fields.Many2one("archive.storage", required=True, readonly=True)
     parent_id = fields.Many2one(
-        'archive.storage',
-        related='transfer_id.dest_storage_id',
-        readonly=True,
+        "archive.storage", related="transfer_id.dest_storage_id", readonly=True
     )
     partner_id = fields.Many2one(
-        'res.partner',
-        related='transfer_id.dest_partner_id',
-        readonly=True,
+        "res.partner", related="transfer_id.dest_partner_id", readonly=True
     )
     location_id = fields.Many2one(
-        'archive.location',
-        related='transfer_id.dest_location_id',
+        "archive.location",
+        related="transfer_id.dest_location_id",
         readonly=True,
     )
     start_date = fields.Datetime(
-        readonly=True,
-        default=lambda r: fields.Datetime.now()
+        readonly=True, default=lambda r: fields.Datetime.now()
     )
-    transfer_id = fields.Many2one(
-        'archive.storage.transfer',
-        readonly=True,
-    )
+    transfer_id = fields.Many2one("archive.storage.transfer", readonly=True)
     end_transfer_id = fields.Many2one(
-        'archive.storage.transfer',
-        readonly=True,
+        "archive.storage.transfer", readonly=True
     )
     end_date = fields.Datetime(readonly=True)
 
-    @api.constrains('child_id', 'transfer_id')
+    @api.constrains("child_id", "transfer_id")
     def _check_transfer(self):
         for rec in self.filtered(lambda r: r.transfer_id):
             if rec.transfer_id.storage_id != rec.child_id:
-                raise ValidationError(_(
-                    'Storage of the transfer must coincide'))
+                raise ValidationError(
+                    _("Storage of the transfer must coincide")
+                )
 
-    @api.constrains('child_id', 'transfer_id', 'end_transfer_id')
+    @api.constrains("child_id", "transfer_id", "end_transfer_id")
     def _check_end_transfer(self):
         for rec in self.filtered(
             lambda r: r.end_transfer_id and r.transfer_id
@@ -326,19 +302,22 @@ class ArchiveStorageParent(models.Model):
             transfer = rec.transfer_id
             end = rec.end_transfer_id
             if end.src_storage_id != transfer.dest_storage_id:
-                raise ValidationError(_(
-                    'Storage of the ending transfer must coincide'))
+                raise ValidationError(
+                    _("Storage of the ending transfer must coincide")
+                )
             if end.src_location_id != transfer.dest_location_id:
-                raise ValidationError(_(
-                    'Location of the ending transfer must coincide'))
+                raise ValidationError(
+                    _("Location of the ending transfer must coincide")
+                )
             if end.src_partner_id != transfer.dest_partner_id:
-                raise ValidationError(_(
-                    'Partner of the ending transfer must coincide'))
+                raise ValidationError(
+                    _("Partner of the ending transfer must coincide")
+                )
 
     def _close_vals(self, transfer):
-        res = {'end_date': fields.Datetime.now()}
+        res = {"end_date": fields.Datetime.now()}
         if transfer:
-            res['end_transfer_id'] = transfer.id
+            res["end_transfer_id"] = transfer.id
         return res
 
     @api.multi
--- archive_management/models/archive_transfer.py
+++ archive_management/models/archive_transfer.py
@@ -2,28 +2,25 @@ from odoo import api, fields, models
 
 
 class ArchiveFileTransfer(models.Model):
-    _name = 'archive.file.transfer'
-    _description = 'archive.file.transfer'
+    _name = "archive.file.transfer"
+    _description = "archive.file.transfer"
 
-    file_id = fields.Many2one(
-        'archive.file',
-        required=True,
-        readonly=True)
-    src_storage_id = fields.Many2one('archive.storage', readonly=True)
-    dest_storage_id = fields.Many2one('archive.storage', readonly=True)
-    src_partner_id = fields.Many2one('res.partner', readonly=True)
-    dest_partner_id = fields.Many2one('res.partner', readonly=True)
+    file_id = fields.Many2one("archive.file", required=True, readonly=True)
+    src_storage_id = fields.Many2one("archive.storage", readonly=True)
+    dest_storage_id = fields.Many2one("archive.storage", readonly=True)
+    src_partner_id = fields.Many2one("res.partner", readonly=True)
+    dest_partner_id = fields.Many2one("res.partner", readonly=True)
 
     @api.multi
     def get_transfer_report(self):
         self.ensure_one()
         res = {
-            'source': self.file_id.name,
-            'date': self.create_date,
-            'user_id': self.create_uid.name,
-            'partner_id': self.dest_partner_id.name or False,
-            'location_id': False,
-            'storage_id': False,
+            "source": self.file_id.name,
+            "date": self.create_date,
+            "user_id": self.create_uid.name,
+            "partner_id": self.dest_partner_id.name or False,
+            "location_id": False,
+            "storage_id": False,
         }
         if self.dest_storage_id:
             res.update(self.dest_storage_id._position_vals(self.create_date))
@@ -31,65 +28,62 @@ class ArchiveFileTransfer(models.Model):
 
 
 class ArchiveMultiStorageTransfer(models.Model):
-    _name = 'archive.multi.storage.transfer'
-    _description = 'archive.multi.storage.transfer'
+    _name = "archive.multi.storage.transfer"
+    _description = "archive.multi.storage.transfer"
 
-    name = fields.Char(
-        required=True,
-        default='/',
-        readonly=True,
-    )
+    name = fields.Char(required=True, default="/", readonly=True)
     transfer_ids = fields.One2many(
-        'archive.storage.transfer',
-        inverse_name='multi_transfer_id',
+        "archive.storage.transfer",
+        inverse_name="multi_transfer_id",
         readonly=True,
     )
-    dest_location_id = fields.Many2one('archive.location', readonly=True)
-    dest_partner_id = fields.Many2one('res.partner', readonly=True)
-    dest_storage_id = fields.Many2one('archive.storage', readonly=True)
+    dest_location_id = fields.Many2one("archive.location", readonly=True)
+    dest_partner_id = fields.Many2one("res.partner", readonly=True)
+    dest_storage_id = fields.Many2one("archive.storage", readonly=True)
 
     @api.model
     def create(self, vals):
-        if vals.get('name', '/') == '/':
-            vals['name'] = self.default_multi_storage_transfer_name(vals)
+        if vals.get("name", "/") == "/":
+            vals["name"] = self.default_multi_storage_transfer_name(vals)
         return super().create(vals)
 
     @api.model
     def default_multi_storage_transfer_name(self, vals):
-        return self.env['ir.sequence'].next_by_code(
-            'archive.multi.storage.transfer') or '/'
+        return (
+            self.env["ir.sequence"].next_by_code(
+                "archive.multi.storage.transfer"
+            )
+            or "/"
+        )
 
 
 class ArchiveStorageTransfer(models.Model):
-    _name = 'archive.storage.transfer'
-    _description = 'archive.storage.transfer'
+    _name = "archive.storage.transfer"
+    _description = "archive.storage.transfer"
 
     multi_transfer_id = fields.Many2one(
-        'archive.multi.storage.transfer',
-        readonly=True,
-        ondelete='restrict',
+        "archive.multi.storage.transfer", readonly=True, ondelete="restrict"
     )
     storage_id = fields.Many2one(
-        'archive.storage',
-        required=True,
-        readonly=True)
-    src_storage_id = fields.Many2one('archive.storage', readonly=True)
-    dest_storage_id = fields.Many2one('archive.storage', readonly=True)
-    src_partner_id = fields.Many2one('res.partner', readonly=True)
-    dest_partner_id = fields.Many2one('res.partner', readonly=True)
-    src_location_id = fields.Many2one('archive.location', readonly=True)
-    dest_location_id = fields.Many2one('archive.location', readonly=True)
+        "archive.storage", required=True, readonly=True
+    )
+    src_storage_id = fields.Many2one("archive.storage", readonly=True)
+    dest_storage_id = fields.Many2one("archive.storage", readonly=True)
+    src_partner_id = fields.Many2one("res.partner", readonly=True)
+    dest_partner_id = fields.Many2one("res.partner", readonly=True)
+    src_location_id = fields.Many2one("archive.location", readonly=True)
+    dest_location_id = fields.Many2one("archive.location", readonly=True)
 
     @api.multi
     def get_transfer_report(self):
         self.ensure_one()
         res = {
-            'source': self.storage_id.name,
-            'date': self.create_date,
-            'user_id': self.create_uid.name,
-            'storage_id': False,
-            'partner_id': self.dest_partner_id.name or False,
-            'location_id': self.dest_location_id.name or False,
+            "source": self.storage_id.name,
+            "date": self.create_date,
+            "user_id": self.create_uid.name,
+            "storage_id": False,
+            "partner_id": self.dest_partner_id.name or False,
+            "location_id": self.dest_location_id.name or False,
         }
         if self.dest_storage_id:
             res.update(self.dest_storage_id._position_vals(self.create_date))
